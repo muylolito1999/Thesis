29c290a4ec5f5a8f5b42ff774fde5a4e2f72b530
Lorenzo Biondi <muylolito1999@gmail.com>
2024-08-01 16:07:38 +0200

    mapped data

---
+++ Python/mapping.py
@@ -0,0 +1,138 @@
+import pandas as pd
+
+# Carica il file CSV specificando il delimitatore corretto
+file_path = '/home/lollo/Thesis/Python/data/data.csv'
+df = pd.read_csv(file_path, delimiter=';')
+
+# Crea il dizionario di mappatura per "Nacionality"
+nationality_mapping = {
+    1: 'Portuguese',
+    2: 'German',
+    6: 'Spanish',
+    11: 'Italian',
+    13: 'Dutch',
+    14: 'English',
+    17: 'Lithuanian',
+    21: 'Angolan',
+    22: 'Cape Verdean',
+    24: 'Guinean',
+    25: 'Mozambican',
+    26: 'Santomean',
+    32: 'Turkish',
+    41: 'Brazilian',
+    62: 'Romanian',
+    100: 'Moldova (Republic of)',
+    101: 'Mexican',
+    103: 'Ukrainian',
+    105: 'Russian',
+    108: 'Cuban',
+    109: 'Colombian'
+}
+
+# Crea il dizionario di mappatura per "Mother's qualification" e "Father's qualification"
+qualification_mapping = {
+    1: 'Secondary Education - 12th Year of Schooling or Eq.',
+    2: "Higher Education - Bachelor's Degree",
+    3: 'Higher Education - Degree',
+    4: "Higher Education - Master's",
+    5: 'Higher Education - Doctorate',
+    6: 'Frequency of Higher Education',
+    9: '12th Year of Schooling - Not Completed',
+    10: '11th Year of Schooling - Not Completed',
+    11: '7th Year (Old)',
+    12: 'Other - 11th Year of Schooling',
+    14: '10th Year of Schooling',
+    18: 'General commerce course',
+    19: 'Basic Education 3rd Cycle (9th/10th/11th Year) or Equiv.',
+    22: 'Technical-professional course',
+    26: '7th year of schooling',
+    27: '2nd cycle of the general high school course',
+    29: '9th Year of Schooling - Not Completed',
+    30: '8th year of schooling',
+    34: 'Unknown',
+    35: "Can't read or write",
+    36: 'Can read without having a 4th year of schooling',
+    37: 'Basic education 1st cycle (4th/5th year) or equiv.',
+    38: 'Basic Education 2nd Cycle (6th/7th/8th Year) or Equiv.',
+    39: 'Technological specialization course',
+    40: 'Higher education - degree (1st cycle)',
+    41: 'Specialized higher studies course',
+    42: 'Professional higher technical course',
+    43: 'Higher Education - Master (2nd cycle)',
+    44: 'Higher Education - Doctorate (3rd cycle)'
+}
+
+# Crea il dizionario di mappatura per "Mother's occupation" e "Father's occupation"
+occupation_mapping = {
+    0: 'Student',
+    1: 'Representatives of the Legislative Power and Executive Bodies, Directors, Directors and Executive Managers',
+    2: 'Specialists in Intellectual and Scientific Activities',
+    3: 'Intermediate Level Technicians and Professions',
+    4: 'Administrative staff',
+    5: 'Personal Services, Security and Safety Workers and Sellers',
+    6: 'Farmers and Skilled Workers in Agriculture, Fisheries and Forestry',
+    7: 'Skilled Workers in Industry, Construction and Craftsmen',
+    8: 'Installation and Machine Operators and Assembly Workers',
+    9: 'Unskilled Workers',
+    10: 'Armed Forces Professions',
+    90: 'Other Situation',
+    99: '(blank)',
+    101: 'Armed Forces Officers',
+    102: 'Armed Forces Sergeants',
+    103: 'Other Armed Forces personnel',
+    112: 'Directors of administrative and commercial services',
+    114: 'Hotel, catering, trade and other services directors',
+    121: 'Specialists in the physical sciences, mathematics, engineering and related techniques',
+    122: 'Health professionals',
+    123: 'Teachers',
+    124: 'Specialists in finance, accounting, administrative organization, public and commercial relations',
+    131: 'Intermediate level science and engineering technicians and professions',
+    132: 'Technicians and professionals, of intermediate level of health',
+    134: 'Intermediate level technicians from legal, social, sports, cultural and similar services',
+    135: 'Information and communication technology technicians',
+    141: 'Office workers, secretaries in general and data processing operators',
+    143: 'Data, accounting, statistical, financial services and registry-related operators',
+    144: 'Other administrative support staff',
+    151: 'Personal service workers',
+    152: 'Sellers',
+    153: 'Personal care workers and the like',
+    154: 'Protection and security services personnel',
+    161: 'Market-oriented farmers and skilled agricultural and animal production workers',
+    163: 'Farmers, livestock keepers, fishermen, hunters and gatherers, subsistence',
+    171: 'Skilled construction workers and the like, except electricians',
+    172: 'Skilled workers in metallurgy, metalworking and similar',
+    174: 'Skilled workers in electricity and electronics',
+    175: 'Workers in food processing, woodworking, clothing and other industries and crafts',
+    181: 'Fixed plant and machine operators',
+    182: 'Assembly workers',
+    183: 'Vehicle drivers and mobile equipment operators',
+    192: 'Unskilled workers in agriculture, animal production, fisheries and forestry',
+    193: 'Unskilled workers in extractive industry, construction, manufacturing and transport',
+    194: 'Meal preparation assistants',
+    195: 'Street vendors (except food) and street service providers'
+}
+
+# Funzione per mappare i valori solo se sono esattamente numeri interi e presenti nel dizionario
+def map_values(value, mapping):
+    try:
+        int_value = int(value)  # Prova a convertire il valore in intero
+        return mapping.get(int_value, value)  # Restituisci la mappatura o il valore originale se non mappato
+    except ValueError:
+        return value  # Restituisci il valore originale se non è un numero intero
+
+# Applica la mappatura alla colonna "Nacionality"
+df['Nacionality'] = df['Nacionality'].apply(lambda x: map_values(x, nationality_mapping))
+
+# Applica la mappatura alle colonne "Mother's qualification" e "Father's qualification"
+df["Mother's qualification"] = df["Mother's qualification"].apply(lambda x: map_values(x, qualification_mapping))
+df["Father's qualification"] = df["Father's qualification"].apply(lambda x: map_values(x, qualification_mapping))
+
+# Applica la mappatura alle colonne "Mother's occupation" e "Father's occupation"
+df["Mother's occupation"] = df["Mother's occupation"].apply(lambda x: map_values(x, occupation_mapping))
+df["Father's occupation"] = df["Father's occupation"].apply(lambda x: map_values(x, occupation_mapping))
+
+# Salva il DataFrame modificato in un nuovo file CSV
+output_file_path = '/home/lollo/Thesis/Python/data/data_mapped.csv'
+df.to_csv(output_file_path, index=False, sep=';')
+
+print("La sostituzione è stata completata con successo!")
